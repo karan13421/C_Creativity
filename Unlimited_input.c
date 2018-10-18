31c8f8e341603aad8f2b46f6f5aeb0fb5b0f54f7
karan13421 <karanrathore7777@gmail.com>
2018-10-19 00:27:51 +0530

    add program

---
+++ Unlimited_input.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    
+    int * arry,limit=9,size=0;
+    char test[5];
+    arry=(int *)malloc(sizeof(int)*10);
+    printf("Enter the values to be sorted (Type your name to sort the values at end");
+    for(;;size++)
+    {
+        if(size>=limit)
+        {
+            arry=(int *)realloc(arry,sizeof(int)*10);
+            if(arry==NULL)
+            {
+                printf("No more allocation");
+                return 0;
+            }
+            limit+=10;
+        }
+        printf("\n %d :",size);
+        scanf("%s",test);
+
+        if(atoi(test))
+            *(arry+size)=(int)atoi(test);
+        else
+            break;
+    }
+
+    for(int i=0;i<=(size-1);i++)
+        printf("%d",arry[i]);
+    
+    return 0;
+}
